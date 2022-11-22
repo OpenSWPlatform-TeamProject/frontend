7743fe4257081bac9cd680aa0e0ab0862dcbc053
Yelynn Oh <yoyelynn@gmail.com>
2022-11-22 18:20:28 +0900

    fix/ add-restaurant 수정

---
+++ static/add-restaurant.js
@@ -0,0 +1,55 @@
+if(document.getElementById("input-check-mon").checked) {
+    document.getElementById("input-check-hidden-mon").disabled = true;
+}
+
+if(document.getElementById("input-check-tues").checked) {
+    document.getElementById("input-check-hidden-tues").disabled = true;
+}
+
+if(document.getElementById("input-check-wed").checked) {
+    document.getElementById("input-check-hidden-wed").disabled = true;
+}
+
+if(document.getElementById("input-check-thur").checked) {
+    document.getElementById("input-check-hidden-thur").disabled = true;
+}
+
+if(document.getElementById("input-check-fri").checked) {
+    document.getElementById("input-check-hidden-fri").disabled = true;
+}
+
+if(document.getElementById("input-check-sat").checked) {
+    document.getElementById("input-check-hidden-sat").disabled = true;
+}
+
+if(document.getElementById("input-check-sun").checked) {
+    document.getElementById("input-check-hidden-sun").disabled = true;
+}
+
+
+
+if(document.getElementById("food-type-check-korean").checked) {
+    document.getElementById("food-type-check-korean").disabled = true;
+}
+
+
+if(document.getElementById("food-type-check-chinese").checked) {
+    document.getElementById("food-type-check-chinese").disabled = true;
+}
+
+if(document.getElementById("food-type-check-western").checked) {
+    document.getElementById("food-type-check-western").disabled = true;
+}
+
+if(document.getElementById("food-type-check-japanese").checked) {
+    document.getElementById("food-type-check-japanese").disabled = true;
+}
+
+if(document.getElementById("food-type-check-cafe").checked) {
+    document.getElementById("food-type-check-cafe").disabled = true;
+}
+
+if(document.getElementById("food-type-check-alcohol").checked) {
+    document.getElementById("food-type-check-alcohol").disabled = true;
+}
+
