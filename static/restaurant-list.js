2104d4b1ec31f76f84efff6388f85c284c1fdc69
Yelynn Oh <yoyelynn@gmail.com>
2022-12-20 02:30:15 +0900

    fix/ 찜 버튼 나머지 수정 및 세부 리뷰 화면 삭제하기 버튼 삭제

--- static/restaurant-list.js
+++ static/restaurant-list.js
@@ -1,7 +1,18 @@
 
-function toggleFavoritesImage(event) {
+function toggleFavoritesImage1(event) {
   event.stopPropagation();
   const objectPosition = event.target.style.objectPosition;
+  console.log('처음이다 이놈아');
+  if (objectPosition === "0px 0px")
+    event.target.style.objectPosition = "0 -30px";
+  else
+    event.target.style.objectPosition = "0 0";
+}
+
+function toggleFavoritesImage2(event) {
+  event.stopPropagation();
+  const objectPosition = event.target.style.objectPosition;
+  console.log('메롱메롱');
   if (objectPosition === "0px 0px")
     event.target.style.objectPosition = "0 -30px";
   else
